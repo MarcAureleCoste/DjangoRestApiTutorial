bc00445c8220053e79e52c1d3d9bf925a00ed28e
Marc-Aurele Coste <c.marc.aurele@outlook.com>
2019-02-24 14:41:46 +0100

    Installation Install Django and Django REST Framework and set the required settings.

---
+++ DjangoRestApiTutorial/settings/production.py
@@ -0,0 +1,14 @@
+"""Production settings"""
+from .common import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
